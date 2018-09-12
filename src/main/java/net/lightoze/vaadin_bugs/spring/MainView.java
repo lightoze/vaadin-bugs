70559f45392d2d9259120467146d57aa639d5d45
Vladimir Kulev <me@lightoze.net>
2018-09-12 16:25:50 +0300

    Add project starter base

---
+++ src/main/java/net/lightoze/vaadin_bugs/spring/MainView.java
@@ -0,0 +1,18 @@
+package net.lightoze.vaadin_bugs.spring;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Route
+public class MainView extends VerticalLayout {
+
+    public MainView(@Autowired MessageBean bean) {
+        Button button = new Button("Click me",
+                e -> Notification.show(bean.getMessage()));
+        add(button);
+    }
+
+}
